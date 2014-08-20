f79fdac07f15dec2a0bd117ad4fe220c9e442194
Mixbo <mixbo@wakowakowako.com>
2014-08-20 11:43:05 -0300

    package manager support to root.sh

--- root.sh
+++ root.sh
@@ -3,24 +3,54 @@
 #	https://github.com/Mixbo/FaST2704/
 #	www.wakowakowako.com
 
-type expect >/dev/null 2>&1 || { echo >&2 "[-] expect is not installed. Closing program..."; exit 1; }
+function main()
+{
+	if haveProg expect; then
+	if [ -z "$1" ]
+	  then
+	    host="192.168.1.1"
+	  else
+	  	host=$1
+	fi
 
-if [ -z "$1" ]
-  then
-    host="192.168.1.1"
-  else
-  	host=$1
-fi
+	(expect -c "
+		set timeout 20
+		spawn telnet $host
+		expect \"Login:\"
+		send \"user\r\"
+		expect \"Password:\"
+		send \"user\r\"
+		expect \">\"
+		send \"ping -c 1 127.0.0.1;bash\n\"
+		interact
+		exit
+	")
+	else
+		install expect
+	fi
+}
 
-(expect -c "
-	set timeout 20
-	spawn telnet $host
-	expect \"Login:\"
-	send \"user\r\"
-	expect \"Password:\"
-	send \"user\r\"
-	expect \">\"
-	send \"ping -c 1 127.0.0.1;bash\n\"
-	interact
+function haveProg()
+{
+    [ -x "$(	which $1)" ]
+}
+
+function install()
+{
+	echo "[-] You need to install $1"
+	if haveProg apt-get ; then 
+		echo "[+] You can install it with:	sudo apt-get install $1"
+	elif haveProg yum ; then 
+		echo "[+] You can install it with:	sudo yum install $1"
+	elif haveProg port ; then 
+		echo "[+] You can install it with:	sudo port install $1"
+	elif haveProg pacman ; then 
+		echo "[+] You can install it with:	sudo pacman -S $1"
+	fi
+
+	echo "[+] Closing..."
 	exit
-")
+}
+
+main $1
+
